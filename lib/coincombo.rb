dd6cbac0bcf777207d0dc836318a551f75ebfc9a
Annie Shin and Will Swanson <will@>
2018-06-28 08:50:48 -0700

    Pass test 1 of 75 cents

--- lib/coincombo.rb
+++ lib/coincombo.rb
@@ -3,6 +3,15 @@ class Coins
     @money = amount
   end
 
+
+
   def calculate()
+    i = 0
+    @money = @money * 100
+    until @money == 0 do
+      @money = @money - 25
+      i += 1
+    end
+    return "#{i} quarters"
   end
 end
