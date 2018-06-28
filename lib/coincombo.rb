310135584ba0fd9e74a94b03cbbeda0c92196d65
Annie Shin and Will Swanson <annie@>
2018-06-28 10:00:47 -0700

    Fail test 2 of 76 cents

--- lib/coincombo.rb
+++ lib/coincombo.rb
@@ -3,15 +3,41 @@ class Coins
     @money = amount
   end
 
-
-
   def calculate()
-    i = 0
+    untilCount = 0
+    # variable untilCount counts the number of times iterating through until loop
+    forCount = 0
+    # variable forCount counts how many times iterating through if loop
+    denominations = [25, 10, 5, 1]
+    # denominations is an array of each coin amount
+    solution = []
+    # solution is an array for the amount of coins for the answer
     @money = @money * 100
+    # changes the cent amount into whole numbers
     until @money == 0 do
-      @money = @money - 25
-      i += 1
+      #condition by which we execute the loop
+      if @money - denominations[untilCount] > 0
+      # if for example, you have 75 cents, and you can subtract 25 cents, setting untilCount to 1, you can loop again. Keep repeating the loop below.
+        @money = @money - denominations[untilCount]
+        forCount += 1
+      else
+        solution[untilCount].push(forCount)
+        #pushing how many times you were able to subtract 25 cents here
+        forCount = 0
+        #if you cannot go subtract 25 cents anymore, you reset forCount HERE, and then it knows to go through next smallest denomination because of the variable below untilCount
+        untilCount += 1
+      end
     end
-    return "#{i} quarters"
+      changeOutput = ""
+      if solution[0] != 0
+        changeOutput = changeOutput + "#{solution[0]} quarters, "
+      elsif solution[1] != 0
+        changeOutput = changeOutput + "#{solution[1]} dimes, "
+      elsif solution[2] != 0
+        changeOutput = changeOutput + "#{solution[2]} nickels, "
+      elsif solution[3] != 0
+        changeOutput = changeOutput + "#{solution[3]} pennies."
+      end
+      return changeOutput
   end
 end
