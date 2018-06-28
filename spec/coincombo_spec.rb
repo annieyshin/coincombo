b8dfa8cdd8995db1913e4ebffb64fca8feed26fd
Annie Shin and Will Swanson <will@>
2018-06-28 08:42:18 -0700

    Fail test 1 of 75 cents

--- spec/coincombo_spec.rb
+++ spec/coincombo_spec.rb
@@ -3,7 +3,7 @@ require('coincombo')
 
 describe('#coincombo') do
   it('returns 3 quarters if 75 cents to make change from is the given argument') do
-    change = Coins.new(.75)
+    change = Coins.new(0.75)
     expect(change.calculate()).to(eq("3 quarters"))
   end
 end
