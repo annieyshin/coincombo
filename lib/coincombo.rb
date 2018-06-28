c2903010c982e568349fe4784d7b493a2ff0256a
Annie Shin and Will Swanson <will@>
2018-06-28 14:31:11 -0700

    it is working holy crap

--- lib/coincombo.rb
+++ lib/coincombo.rb
@@ -1,7 +1,7 @@
 class Coins
   def initialize(amount)
     @money = (amount * 100).to_i
-    puts "1 beginning of run, money equals #{@money}"
+    puts "starting money #{@money}"
   end
 
   def calculate()
@@ -9,43 +9,44 @@ class Coins
     coin_count = 0
     denominations = [25, 10, 5, 1]
     solution = [0, 0, 0, 0]
-    until @money <= 0
-      if (denomination_index == 3)
-        puts "2 got to index 3 if step"
-        until @money == 0
-          @money = @money - denominations[denomination_index]
-          coin_count += 1
-          solution[denomination_index] = coin_count
-        end
-      else
-        puts "3 got to else step for if loop"
-        if (@money < 5 && @money > 0) || @money == 0
-          puts "3.5 got inside of until loop"
-          if (@money - denominations[denomination_index] >= 0)
-            puts "4 got to if loop inside of else step"
-            #  ||
-            @money = @money - denominations[denomination_index]
-            coin_count += 1
-            puts "5 money = #{@money}, denomination_index = #{denomination_index}, and coin_count = #{coin_count}"
-              solution[denomination_index] = coin_count
-          else
-            puts "6 got to else inside of else"
-            solution[denomination_index] = coin_count
-            coin_count = 0
-            denomination_index += 1
+    denominations.each do |step|
+      if step == 25 || step == 10 || step == 5
+          puts "#{@money}"
+          puts step
+          puts denomination_index
+          if (@money - step >= 0)
+            puts "big if loop"
+            until @money - step < 0
+              puts "big until loop"
+              coin_count += 1
+              @money = @money - step
+            end
           end
+          puts "big else"
+          puts "money in big else #{@money}"
+          solution[denomination_index] = coin_count
+          coin_count = 0
+          denomination_index += 1
+      else
+        puts "little else"
+        coin_count = 0
+        until @money == 0
+          puts "little until"
+          coin_count += 1
+          @money = @money - step
+          solution[3] = coin_count
         end
       end
     end
     changeOutput = ""
     if solution[0] != 0
-      changeOutput = changeOutput + "#{solution[0]} quarters, "
+      changeOutput = changeOutput + "#{solution[0]} quarters "
     end
     if solution[1] != 0
-      changeOutput = changeOutput + "#{solution[1]} dimes, "
+      changeOutput = changeOutput + "#{solution[1]} dimes "
     end
     if solution[2] != 0
-      changeOutput = changeOutput + "#{solution[2]} nickels, "
+      changeOutput = changeOutput + "#{solution[2]} nickels "
     end
     if solution[3] != 0
       changeOutput = changeOutput + "#{solution[3]} pennies."
@@ -53,7 +54,3 @@ class Coins
     return changeOutput
   end
 end
-#
-# puts "hello"
-# change = Coins.new(0.77)
-# change.calculate()
--- spec/coincombo_spec.rb
+++ spec/coincombo_spec.rb
@@ -7,12 +7,16 @@ describe('#coincombo') do
     change = Coins.new(0.02)
     expect(change.calculate()).to(eq("2 pennies."))
   end
+  it('returns 1 quarter 1 dime 1 nickel and 2 pennies if 47 cents is the given argument') do
+    change = Coins.new(0.42)
+    expect(change.calculate()).to(eq("1 quarters 1 dimes 1 nickels 2 pennies."))
+  end
   it('returns 3 quarters if 75 cents to make change from is the given argument') do
     change = Coins.new(0.75)
-    expect(change.calculate()).to(eq("3 quarters, "))
+    expect(change.calculate()).to(eq("3 quarters "))
   end
   it('returns 3 quarters and 2 pennies if 77 cents to make change from is the given argument') do
     change = Coins.new(0.77)
-    expect(change.calculate()).to(eq("3 quarters, 2 pennies."))
+    expect(change.calculate()).to(eq("3 quarters 2 pennies."))
   end
 end
